98f51cbfb594b66543f4c3b17267ef5eaf44f7c4
NoNelso <jasonr4nd@gmail.com>
2018-12-19 14:15:44 -0500

    added missing this. to vars

--- Star.js
+++ Star.js
@@ -1,4 +1,4 @@
-function Star() {
+function star() {
   this.x = random(-width, width);
   this.y = random(-height, height)
   this.z = random(width)
@@ -18,10 +18,10 @@ this.update = function() {
 this.show = function() {
   fill(255);
   noStroke();
-  var sx = map(x / z, 0, 1, 0, width);
-  var sy = map(y / x, 0, 1, 0, height);
-  var r = map(z, 0, width, 16, 0);
-  ellipse(sx, sy, 8, 8);
+  var sx = map(this.x / this.z, 0, 1, 0, width);
+  var sy = map(this.y / this.x, 0, 1, 0, height);
+  var r = map(this.z, 0, width, 16, 0);
+  ellipse(this.sx, this.sy, 8, 8);
   var px = map(this.x / this.pz, 0, 1, 0, width);
   var py = map(this.y / this.pz, 0, 1, 0, height);
   this.pz = this.z;
