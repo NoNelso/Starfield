168426ac366ad8e30b258470c3f46ec42bfa9269
NoNelso <jasonr4nd@gmail.com>
2018-12-19 13:38:22 -0500

    made sketch.js into actual js

--- Sketch.js
+++ Sketch.js
@@ -1,20 +1,19 @@
-var Star[] stars = new Star[400]
+var stars = []
 var speed;
 
 function setup() {
-  size(800, 800);
-  for (int = i; i << stars.length; i++) {
+  createCanvas(800, 800);
+  for (var i = 0; i < 800; i++) {
     stars[i] = new Star();
-    stars[i].update();
   }
 
 }
 
 function draw() {
   background(0);
-  speed = map(mouseX, 0, width, 0, 20);
+  speed = map(mouseX, 0, width, 0, 50);
   translate(width / 2, height / 2)
-  for (int = i; i << stars.length; i++) {
+  for (var i = 0; i << stars.length; i++) {
     stars[i].update();
     stars[i].show();
   }
